d70e9905278656722f840c73034d56d87eb002fb
shawncocklin <shawncocklin@gmail.com>
2022-06-29 15:12:13 -0400

    got leaderboard generated through JS

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // TODO: refactor code
 // TODO: page animations
 
-
 import { createClient } from '@supabase/supabase-js'
 
 const url = import.meta.env.VITE_SUPABASE_URL
@@ -11,6 +10,7 @@ const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(url, anonKey)
 // DOM reference variables
 const bookContainer = document.querySelector('#book-container')
+const leaderboardContainer = document.querySelector('#leaderboard-container')
 const titleInput = document.querySelector('#title')
 const authorInput = document.querySelector('#author')
 const readerInput = document.querySelector('#reader')
@@ -19,6 +19,8 @@ const leaderBtn = document.querySelector('#leaderboard-btn')
 const submitBtn = document.querySelector('.btn-submit')
 const validationText = document.querySelector('.validation-text')
 
+// initialize and generate HTML for the leaderboard component
+leaderboardInit()
 
 // event listeners
 document.addEventListener('DOMContentLoaded', displayBooks)
@@ -32,7 +34,6 @@ function Book(title, author, pageCount, reader) {
   this.author = author
   this.pageCount = pageCount
   this.reader = reader
-  
 }
 
 // generate HTML for the displayed book cards
@@ -115,70 +116,106 @@ function generateBookCard(title, author, pageCount, index, date, reader) {
   `
 }
 
+function generateLeaderboard() {
+  return `
+    <div class="leaderboard bg-primary flow hide">
+      <p class="font-sans text-light fs-700 fw-semibold text-center">Leaderboard</p>
+      <div class="flex justify-between">
+        <p class="font-sans text-light fs-500">Name</p>
+        <p class="font-sans text-light fs-500">Books</p>
+        <p class="font-sans text-light fs-500">Pages</p>
+      </div>
+      <div class="stats flex justify-between">
+        <p class="font-sans text-light fs-500">Shawn</p>
+        <p class="font-sans text-light fs-500">2</p>
+        <p class="font-sans text-light fs-500">600</p>
+      </div>
+
+    </div>
+  `
+}
+
 // get new book info from user input
 function getBookFromUser() {
-    const title = titleInput.value
-    const author = authorInput.value
-    const pageCount = pageCountInput.value
-    const reader = readerInput.value 
-    return new Book(title, author, pageCount, reader)
+  const title = titleInput.value
+  const author = authorInput.value
+  const pageCount = pageCountInput.value
+  const reader = readerInput.value
+  return new Book(title, author, pageCount, reader)
+}
+
+function leaderboardInit() {
+  const leaderboard = generateLeaderboard()
+  leaderboardContainer.innerHTML = leaderboard
 }
 
 function toggleLeaderboard(e) {
   e.preventDefault()
-  const leaderboard = document.querySelector('.leaderboard')
-  if(!leaderboard.classList.contains('hide')) {
+  const leaderboardElem = document.querySelector('.leaderboard')
+  if (!leaderboardElem.classList.contains('hide')) {
     leaderBtn.textContent = 'Leaderboard'
   } else {
     leaderBtn.textContent = 'Close'
   }
-  leaderboard.classList.toggle('hide')
+  leaderboardElem.classList.toggle('hide')
 }
 
 // add new book to the library array. will be hooked into database eventually
 async function addBookToLibrary(e) {
   e.preventDefault()
-  if(!validationText.classList.contains('hidden')) {
+  if (!validationText.classList.contains('hidden')) {
     validationText.classList.add('hidden')
-  } 
+  }
   bookContainer.innerHTML = ''
   const newBook = getBookFromUser()
-  if(newBook.title === '' || newBook.author === '' || newBook.pageCount === null || newBook.reader === '') {
+  if (
+    newBook.title === '' ||
+    newBook.author === '' ||
+    newBook.pageCount === null ||
+    newBook.reader === ''
+  ) {
     validationText.classList.remove('hidden')
 
     displayBooks()
     return
   }
-  const {data, error} = await supabase.from('books').insert([{
-    title: newBook.title,
-    author: newBook.author,
-    pages: newBook.pageCount,
-    reader: newBook.reader,
-  }])
- 
+  const { data, error } = await supabase.from('books').insert([
+    {
+      title: newBook.title,
+      author: newBook.author,
+      pages: newBook.pageCount,
+      reader: newBook.reader,
+    },
+  ])
+
   titleInput.value = ''
   authorInput.value = ''
   readerInput.value = ''
   pageCountInput.value = ''
 
   displayBooks()
-
 }
 
 // display the book cards to the page
 async function displayBooks() {
   const { data, error } = await supabase.from('books').select()
   data.forEach((book) => {
-
     // massage date object for better readability
     const rawDate = book.created_at
     const date = rawDate.slice(5, 10)
 
     // generate HTML for the book card
-    const displayedBook = generateBookCard(book.title, book.author, book.pages, book.id, date, book.reader)
-    // create div to hold 
+    const displayedBook = generateBookCard(
+      book.title,
+      book.author,
+      book.pages,
+      book.id,
+      date,
+      book.reader
+    )
+    // create div to hold
     const bookCard = document.createElement('div')
-    
+
     bookCard.innerHTML = displayedBook
     bookContainer.appendChild(bookCard)
   })
@@ -187,14 +224,13 @@ async function displayBooks() {
 // handles removing book card from page when delete button pressed
 async function editBookCard(e) {
   const target = e.target
-  if(target.classList.contains('delete-btn')) {
+  if (target.classList.contains('delete-btn')) {
     let bookToRemove = target.closest('.book')
     let bookIndex = bookToRemove.dataset.index
-    const { data, error } = await supabase.from('books').delete().match({ id: bookIndex })
+    const { data, error } = await supabase
+      .from('books')
+      .delete()
+      .match({ id: bookIndex })
     bookToRemove.remove()
   }
 }
-
-
-
-
